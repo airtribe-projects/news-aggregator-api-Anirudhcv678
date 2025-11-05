a1cd2bb3bd4a13345cc1affcc9facbfdddf635ef
Anirudh C V <anirudhjyothiprakash@gmail.com>
2025-11-05 14:15:09 +0530

    test env

--- src/app.ts
+++ src/app.ts
@@ -19,20 +19,32 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     }
 });
 
-mongoose.connect('mongodb://localhost:27017/news-aggregator')
-    .then(() => {
-        console.log('Connected to MongoDB');
-        app.listen(port, (err?: Error): void => {
-            if (err) {
-                return console.log('Something bad happened', err);
-            }
-            console.log(`Server is listening on ${port}`);
+// Only start server if not in test environment
+if (process.env.NODE_ENV !== 'test') {
+    mongoose.connect('mongodb://localhost:27017/news-aggregator')
+        .then(() => {
+            console.log('Connected to MongoDB');
+            app.listen(port, (err?: Error): void => {
+                if (err) {
+                    return console.log('Something bad happened', err);
+                }
+                console.log(`Server is listening on ${port}`);
+            });
+        })
+        .catch(err => {
+            console.error('Failed to connect to MongoDB', err);
+            process.exit(1);
         });
-    })
-    .catch(err => {
-        console.error('Failed to connect to MongoDB', err);
-        process.exit(1);
-    });
+} else {
+    // In test environment, connect to MongoDB but don't start server
+    mongoose.connect('mongodb://localhost:27017/news-aggregator')
+        .then(() => {
+            console.log('Connected to MongoDB (test mode)');
+        })
+        .catch(err => {
+            console.error('Failed to connect to MongoDB', err);
+        });
+}
 
 export default app;
 
